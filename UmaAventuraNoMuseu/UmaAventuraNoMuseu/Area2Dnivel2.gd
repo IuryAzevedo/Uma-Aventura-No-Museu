23473c2f2e62d06664c86c8edc6927a872f40601
Iury Azevedo <89524771+IuryAzevedo@users.noreply.github.com>
2022-11-26 12:51:00 -0300

    Add files via upload

---
+++ UmaAventuraNoMuseu/UmaAventuraNoMuseu/Area2Dnivel2.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+var player = null
+var canInteract: bool = true
+
+func unfreeze():
+	player.set_physics_process(true)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+
+func _process(delta):
+	if Input.is_action_just_pressed("interagir") and player != null and canInteract:
+		canInteract = false
+		var cena = preload("res://info1nivel2.tscn").instance()
+		get_parent().get_node("CanvasLayer").add_child(cena)
+		cena.connect("closed", self, "unfreeze")
+		player.set_physics_process(false)
+		player.get_node("Animacao").play("parada")
+
+func _on_Area2D_body_entered(body):
+	player = body
+	
+
+
+func _on_Area2D_body_exited(body):
+	player = null
+	canInteract = true
